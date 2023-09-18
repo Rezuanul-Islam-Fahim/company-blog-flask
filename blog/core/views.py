db77770969d2d51c591bcec75f36d7e0184518aa
Rezuanul Islam Fahim <rifahim98@gmail.com>
2023-09-18 22:48:14 +0600

    Add info page

--- blog/core/views.py
+++ blog/core/views.py
@@ -5,3 +5,8 @@ core = Blueprint('core', __name__)
 @core.route('/')
 def home():
     return render_template('home.html')
+
+
+@core.route('/info')
+def info():
+    return render_template('info.html')
