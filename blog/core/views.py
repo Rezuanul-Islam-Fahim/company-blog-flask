7e566edfc37f89b11e2ab231d0473406bb0892c4
Rezuanul Islam Fahim <rifahim98@gmail.com>
2023-09-18 21:38:45 +0600

    Add base html file

--- blog/core/views.py
+++ blog/core/views.py
@@ -3,5 +3,5 @@ from flask import Flask, Blueprint, render_template
 core = Blueprint('core', __name__)
 
 @core.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
