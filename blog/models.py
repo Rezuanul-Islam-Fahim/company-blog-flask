6a4748c3a331eaada3783ca90482906cbd0eaaac
Rezuanul Islam Fahim <rifahim98@gmail.com>
2023-09-19 22:42:31 +0600

    Implement Post model

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,14 @@
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from blog import db
 from blog import login_manager
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
+
 
 class User(db.Model, UserMixin):
 
@@ -20,7 +23,7 @@ class User(db.Model, UserMixin):
         default='default_profile.jpg'
     )
     password_hash = db.Column(db.String(128))
-    posts = db.Relationship('Post', backref='author', lazy=True)
+    posts = db.relationship('Post', backref='author', lazy=True)
 
     def __init__(self, email, username, password):
         self.email = email
@@ -29,3 +32,28 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return 'Username: {self.username}, Email: {self.email}'
+
+
+class Post(db.Model):
+
+    __tablename__ = 'posts'
+
+    users = db.relationship(User)
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140), nullable=False)
+    desc = db.Column(db.Text, nullable=False)
+    author_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id'),
+        nullable=False
+    )
+    datetime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, title, desc, user_id):
+        self.title = title
+        self.desc = desc
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f'Title: {self.title}, datetime: {self.datetime}, desc: {self.desc}'
