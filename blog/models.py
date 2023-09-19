ead8d7ea25240aec08f8c8d6fe51daaee7e9d285
Rezuanul Islam Fahim <rifahim98@gmail.com>
2023-09-19 22:15:25 +0600

    Complete user model

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,21 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from blog import db
 
+
 class User(db.Model):
 
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
-    username= db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    profile_img = db.Column(
+        db.String(64),
+        nullable=False,
+        default='default_profile.jpg'
+    )
     password_hash = db.Column(db.String(128))
+    posts = db.Relationship('Post', backref='author', lazy=True)
 
     def __init__(self, email, username, password):
         self.email = email
