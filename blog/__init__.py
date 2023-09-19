a92dce1f1f56e2d5e8ae2bf94f95f921d0d59a75
Rezuanul Islam Fahim <rifahim98@gmail.com>
2023-09-19 20:10:55 +0600

    Initialize database

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,8 +1,21 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from blog.core.views import core
 from blog.error.handler import error
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
+app.config.from_mapping(
+    SECRET_KEY='my_secret_key',
+    SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(basedir, 'data.sqlite'),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+)
+
+db = SQLAlchemy(app)
+Migrate(app, db)
 
 app.register_blueprint(core)
 app.register_blueprint(error)
---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from blog import db
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username= db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    def __init__(self, email, username, password):
+        self.email = email
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    def __repr__(self):
+        return 'Username: {self.username}, Email: {self.email}'
