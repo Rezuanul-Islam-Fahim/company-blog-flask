a5699c917ecdf74decdbc740686b8e01403349ba
Rezuanul Islam Fahim <rifahim98@gmail.com>
2023-09-22 19:53:18 +0600

    Create Register form

--- blog/auth/forms.py
+++ blog/auth/forms.py
@@ -9,3 +9,19 @@ class Login(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+class Register(FlaskForm):
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField(
+        'Password',
+        validators=[
+            DataRequired(),
+            EqualTo('pass_confirm', message='Passwords must match')
+        ]
+    )
+    pass_confirm = PasswordField(
+        'Confirm Password', validators=[DataRequired()]
+    )
