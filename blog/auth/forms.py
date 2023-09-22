381b7bdd8576ab0c56b80300632861a4bbade56b
Rezuanul Islam Fahim <rifahim98@gmail.com>
2023-09-22 19:44:25 +0600

    Create login form

---
+++ blog/auth/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import ValidationError
+
+
+class Login(FlaskForm):
+
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
