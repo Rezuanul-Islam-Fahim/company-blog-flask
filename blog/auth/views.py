6e6233a683e92f6ebe095c15911fe22fb8631292
Rezuanul Islam Fahim <rifahim98@gmail.com>
2023-09-22 23:24:50 +0600

    Implement registration view

--- blog/auth/views.py
+++ blog/auth/views.py
@@ -1,3 +1,29 @@
-from flask import Blueprint
+from flask import Blueprint, flash, redirect, url_for, render_template
+from forms import Login, Register, UpdateUser
+from blog.models import User
+from blog import db
 
 auth = Blueprint('auth', __name__)
+
+
+@auth.route('/register', methods=['POST', 'GET'])
+def register():
+
+    form = Register()
+
+    if form.validate_on_submit():
+
+        new_user = User(
+            email=form.email.data,
+            username=form.username.data,
+            password=form.password.data
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Registration successful')
+
+        return redirect(url_for('auth.login'))
+
+    return render_template('register.html')
