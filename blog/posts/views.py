cf371272f74f22bfb73daa4aad4aeb45653447ac
Rezuanul Islam Fahim <rifahim98@gmail.com>
2023-10-04 23:12:40 +0600

    Create user post view

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,3 +1,16 @@
 from flask import Blueprint, render_template, request, url_for
 
+from blog.models import User, Post
+
 posts = Blueprint('posts', __name__)
+
+
+@posts.route('/<username>')
+def user_posts(username):
+
+    user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.filter_by(author=user).order_by(
+        Post.datetime.desc()).paginate(page=page, per_page=5)
+
+    return render_template('user-blog-posts.html')
