b275b18afe9dfc0e430a64f43a49fe040a8eae08
Rezuanul Islam Fahim <rifahim98@gmail.com>
2023-10-17 22:04:10 +0600

    Update home view and add all collection of post

--- blog/core/views.py
+++ blog/core/views.py
@@ -1,10 +1,14 @@
-from flask import Blueprint, render_template 
+from flask import Blueprint, render_template, request
+from blog.models import Post
 
 core = Blueprint('core', __name__)
 
 @core.route('/')
 def home():
-    return render_template('home.html')
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.datetime.desc()).paginate(page=page, per_page=5)
+
+    return render_template('home.html', posts=posts)
 
 
 @core.route('/info')
