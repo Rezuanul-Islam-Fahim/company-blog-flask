d186cd4b771473d7e84bad1ff77489101eaa48e9
Rezuanul Islam Fahim <rifahim98@gmail.com>
2023-10-22 00:45:23 +0600

    Update database url based on starting part

--- blog/__init__.py
+++ blog/__init__.py
@@ -5,10 +5,14 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from blog.error.handler import error
 
+database_uri = os.environ.get('DATABASE_URL')
+if database_uri and database_uri.startswith('postgres://'):
+    database_uri = database_uri.replace('postgres://', 'postgresql://')
+
 app = Flask(__name__)
 app.config.from_mapping(
     SECRET_KEY=os.environ.get('SECRET_KEY'),
-    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL'),
+    SQLALCHEMY_DATABASE_URI=database_uri,
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 )
 
