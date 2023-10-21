1d3fba1e0bbb789bdcaa753d4316740e583526b0
Rezuanul Islam Fahim <rifahim98@gmail.com>
2023-10-22 00:23:59 +0600

    Update config database var name

--- blog/__init__.py
+++ blog/__init__.py
@@ -8,7 +8,7 @@ from blog.error.handler import error
 app = Flask(__name__)
 app.config.from_mapping(
     SECRET_KEY=os.environ.get('SECRET_KEY'),
-    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URI'),
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL'),
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 )
 
