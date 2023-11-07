2515d84c536c188f65572c68dcff02db1534cb4c
Rezuanul Islam Fahim <rifahim98@gmail.com>
2023-11-07 20:26:52 +0600

    Updated file name

--- blog/api/__init__.py
+++ blog/api/__init__.py
@@ -2,7 +2,7 @@ from flask_restful import Api
 from flask_jwt import JWT
 from .. import app
 from .resources.register import RegisterApi
-from .jwt_handler import authentication, identity
+from .jwt_secure_handler import authentication, identity
 
 api = Api(app)
 JWT(app, authentication, identity)
