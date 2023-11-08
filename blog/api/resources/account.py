880e059bbb19c5a144597df21f8fa57238374a60
Rezuanul Islam Fahim <rifahim98@gmail.com>
2023-11-08 22:55:25 +0600

    Implement /account api - when user not found with provided id

--- blog/api/resources/account.py
+++ blog/api/resources/account.py
@@ -1,7 +1,19 @@
+from flask import request, make_response, jsonify
 from flask_restful import Resource
+from flask_jwt import jwt_required
+from ...models import User
 
 
 class AccountApi(Resource):
 
     def get(self):
-        print('Account api working')
+        user_id = request.args.get('user_id')
+        user = User.query.get(user_id)
+
+        if user:
+            pass
+        else:
+            return make_response(
+                jsonify(error={'message': f'No user found with id {user_id}'}),
+                404
+            )
