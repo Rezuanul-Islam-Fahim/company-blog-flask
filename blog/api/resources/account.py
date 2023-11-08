2aa039a89cf53ed3594c9429961870286867884e
Rezuanul Islam Fahim <rifahim98@gmail.com>
2023-11-09 01:40:39 +0600

    Implement /account - put method for updating account through api

--- blog/api/resources/account.py
+++ blog/api/resources/account.py
@@ -2,6 +2,7 @@ from flask import request, make_response, jsonify
 from flask_restful import Resource
 from flask_jwt import jwt_required, current_identity
 from ...models import User
+from ... import db
 
 
 class AccountApi(Resource):
@@ -21,6 +22,44 @@ class AccountApi(Resource):
 
         elif user:
             if user.id == current_identity.id:
+                return jsonify(user=user.json())
+            else:
+                return make_response(
+                    jsonify(error={'message': 'Permission denied'}),
+                    403
+                )
+
+        else:
+            return make_response(
+                jsonify(error={'message': f'No user found with id {user_id}'}),
+                404
+            )
+
+    @jwt_required()
+    def put(self):
+
+        user_id = request.args.get('user_id')
+        req_json = request.get_json()
+        user = User.query.get(user_id)
+
+        if user_id is None:
+            return make_response(
+                jsonify(
+                    error={'message': 'Please provide (user_id) parameter'}
+                ),
+                405
+            )
+
+        elif user:
+            if user.id == current_identity.id:
+                if req_json.get('username') is not None:
+                    user.username = req_json.get('username')
+                if req_json.get('email') is not None:
+                    user.email = req_json.get('email')
+
+                db.session.add(user)
+                db.session.commit()
+
                 return jsonify(user=user.json())
             else:
                 return make_response(
