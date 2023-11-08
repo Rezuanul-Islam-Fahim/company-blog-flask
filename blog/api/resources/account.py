c7e9a7b50ebf8fe9a9ae0e81bb17102732d50e75
Rezuanul Islam Fahim <rifahim98@gmail.com>
2023-11-08 23:06:33 +0600

    Make JWT authorization required on /account api

--- blog/api/resources/account.py
+++ blog/api/resources/account.py
@@ -1,17 +1,24 @@
 from flask import request, make_response, jsonify
 from flask_restful import Resource
-from flask_jwt import jwt_required
+from flask_jwt import jwt_required, current_identity
 from ...models import User
 
 
 class AccountApi(Resource):
 
+    @jwt_required()
     def get(self):
         user_id = request.args.get('user_id')
         user = User.query.get(user_id)
 
         if user:
-            return jsonify(user=user.json())
+            if user.id == current_identity.id:
+                return jsonify(user=user.json())
+            else:
+                return make_response(
+                    jsonify(error={'message': 'Permission denied'}),
+                    403
+                )
 
         else:
             return make_response(
