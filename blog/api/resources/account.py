1f02dfdf12c673b66106e5385711ff3ce1c8fd3c
Rezuanul Islam Fahim <rifahim98@gmail.com>
2023-11-10 21:08:52 +0600

    Updated Account Api to get user from jwt current_identity

--- blog/api/resources/account.py
+++ blog/api/resources/account.py
@@ -1,4 +1,4 @@
-from flask import request, make_response, jsonify
+from flask import request, jsonify
 from flask_restful import Resource
 from flask_jwt import jwt_required, current_identity
 from ...models import User
@@ -9,66 +9,21 @@ class AccountApi(Resource):
 
     @jwt_required()
     def get(self):
-        user_id = request.args.get('user_id')
-        user = User.query.get(user_id)
 
-        if user_id is None:
-            return make_response(
-                jsonify(
-                    error={'message': 'Please provide (user_id) parameter'}
-                ),
-                405
-            )
-
-        elif user:
-            if user.id == current_identity.id:
-                return jsonify(user=user.json())
-            else:
-                return make_response(
-                    jsonify(error={'message': 'Permission denied'}),
-                    403
-                )
-
-        else:
-            return make_response(
-                jsonify(error={'message': f'No user found with id {user_id}'}),
-                404
-            )
+        user = User.query.get(current_identity.id)
+        return jsonify(user=user.json())
 
     @jwt_required()
     def put(self):
-
-        user_id = request.args.get('user_id')
         req_json = request.get_json()
-        user = User.query.get(user_id)
+        user = User.query.get(current_identity.id)
 
-        if user_id is None:
-            return make_response(
-                jsonify(
-                    error={'message': 'Please provide (user_id) parameter'}
-                ),
-                405
-            )
+        if req_json.get('username') is not None:
+            user.username = req_json.get('username')
+        if req_json.get('email') is not None:
+            user.email = req_json.get('email')
 
-        elif user:
-            if user.id == current_identity.id:
-                if req_json.get('username') is not None:
-                    user.username = req_json.get('username')
-                if req_json.get('email') is not None:
-                    user.email = req_json.get('email')
+        db.session.add(user)
+        db.session.commit()
 
-                db.session.add(user)
-                db.session.commit()
-
-                return jsonify(user=user.json())
-            else:
-                return make_response(
-                    jsonify(error={'message': 'Permission denied'}),
-                    403
-                )
-
-        else:
-            return make_response(
-                jsonify(error={'message': f'No user found with id {user_id}'}),
-                404
-            )
+        return jsonify(user=user.json())
